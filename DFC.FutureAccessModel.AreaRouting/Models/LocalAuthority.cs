dae38821ebf13afd1f5d902457c91cd96b2ed60f
Joe Hudson-Small <Joehs@me.com>
2023-03-30 11:00:05 +0100

    Adds NCSLT-3714 to Release 34.1 (#18)
    * core spec change...
    
    for increased success using step back process from postcode to outward code
    
    * postman script updates
    
    * Updated to permit TouchpointID instead of "id" as a valid POST property
    
    * .Net 6 upgrade
    
    * core spec change...
    
    for increased success using step back process from postcode to outward code
    
    * postman script updates
    
    * Updated to permit TouchpointID instead of "id" as a valid POST property
    
    * .Net 6 upgrade
    
    * Updated ARM template to v4
    
    ---------
    
    Co-authored-by: colin <msdn@thestripedlawn.co>
    Co-authored-by: Ian Crisp <icrisp@program-systems.co.uk>
    Co-authored-by: Zhaoming LU <Zhaoming.LU@EDUCATION.GOV.UK>
    Co-authored-by: Amandeep Sandhu <purpleduckmedia@gmail.com>

--- DFC.FutureAccessModel.AreaRouting/Models/LocalAuthority.cs
+++ DFC.FutureAccessModel.AreaRouting/Models/LocalAuthority.cs
@@ -1,4 +1,6 @@
-﻿namespace DFC.FutureAccessModel.AreaRouting.Models
+﻿using Newtonsoft.Json;
+
+namespace DFC.FutureAccessModel.AreaRouting.Models
 {
     /// <summary>
     /// the local authority (intermediary class)
@@ -9,32 +11,17 @@
         /// <summary>
         /// the local admin district code
         /// </summary>
-        //[Key]
-        //[Required]
-        //[JsonProperty("id")]
-        //[RegularExpression(ValidationExpressions.)]
-        //[Display(Description = "The authority's unique identifier")]
-        //[StringLength(10, MinimumLength = 10)]
-        //[Example(Description = "E09000002")]
+        [JsonProperty("id")]
         public string LADCode { get; set; }
 
         /// <summary>
         /// the (authorities) touchpoint
         /// </summary>
-        //[Required]
-        //[Display(Description = "The authority's touchpoint")]
-        //[StringLength(10, MinimumLength = 10)]
-        //[Example(Description = "0000000101")]
         public string TouchpointID { get; set; }
 
         /// <summary>
         /// the (authority) name
         /// </summary>
-        //[Required]
-        //[StringLength(50)]
-        //[RegularExpression(ValidationExpressions.TownOrRegion)]
-        //[Display(Description = "The name of the authority")]
-        //[Example(Description = "Barking and Dagenham")]
         public string Name { get; set; }
     }
 }
